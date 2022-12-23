be25dcd60ac9d5d27a724ae95f24d0b1aae21d59
quxiangyu01 <542753177@qq.com>
2022-12-24 02:01:19 +0800

    add

---
+++ padding trick/wmt14/trg/bin_packing.py
@@ -0,0 +1,347 @@
+from __future__ import print_function
+
+
+def packing1D(d,
+              V_max,
+              weight_pos=None,
+              key=None,
+              lower_bound=None,
+              upper_bound=None,
+              ):
+    """
+    Distributes a list of weights, a dictionary of weights or a list of tuples containing weights
+    to a minimal number of bins that have a fixed volume.
+
+    Parameters
+    ==========
+    d : iterable
+        list containing weights,
+        OR dictionary where each (key,value)-pair carries the weight as value,
+        OR list of tuples where one entry in the tuple is the weight. The position of
+        this weight has to be given in optional variable weight_pos
+    V_max : int or float
+        Fixed bin volume
+    weight_pos : int, default = None
+        if d is a list of tuples, this integer number gives the position of the weight in a tuple
+    key : function, default = None
+        if d is a list, this key functions grabs the weight for an item
+    lower_bound : float, default = None
+        weights under this bound are not considered
+    upper_bound : float, default = None
+        weights exceeding this bound are not considered
+
+    Returns
+    =======
+    bins : list
+        A list. Each entry is a list of items or
+        a dict of items, depending on the type of ``d``.
+    """
+
+    def get(lst, ndx):
+        return [lst[n] for n in ndx]
+
+    def argmax(lst):
+        return max(range(len(lst)), key=lst.__getitem__)
+
+    def revargsort(lst):
+        return sorted(range(len(lst)), key=lambda i: -lst[i])
+
+    isdict = isinstance(d, dict)
+
+    if not hasattr(d, '__len__'):
+        raise TypeError("d must be iterable")
+
+    if not isdict and hasattr(d[0], '__len__'):
+        if weight_pos is not None:
+            key = lambda x: x[weight_pos]
+        if key is None:
+            raise ValueError("Must provide weight_pos or key for tuple list")
+
+    if not isdict and key:
+        new_dict = {i: val for i, val in enumerate(d)}
+        d = {i: key(val) for i, val in enumerate(d)}
+        isdict = True
+        is_tuple_list = True
+    else:
+        is_tuple_list = False
+
+    if isdict:
+
+        # get keys and values (weights)
+        keys_vals = d.items()
+        keys = [k for k, v in keys_vals]
+        vals = [v for k, v in keys_vals]
+
+        # sort weights decreasingly
+        ndcs = revargsort(vals)
+
+        weights = get(vals, ndcs)
+        keys = get(keys, ndcs)
+
+        bins = [{}]
+    else:
+        weights = sorted(d, key=lambda x: -x)
+        bins = [[]]
+
+    # find the valid indices
+    if lower_bound is not None and upper_bound is not None and lower_bound < upper_bound:
+        valid_ndcs = filter(lambda i: lower_bound < weights[i] < upper_bound, range(len(weights)))
+    elif lower_bound is not None:
+        valid_ndcs = filter(lambda i: lower_bound < weights[i], range(len(weights)))
+    elif upper_bound is not None:
+        valid_ndcs = filter(lambda i: weights[i] < upper_bound, range(len(weights)))
+    elif lower_bound is None and upper_bound is None:
+        valid_ndcs = range(len(weights))
+    elif lower_bound >= upper_bound:
+        raise Exception("lower_bound is greater or equal to upper_bound")
+
+    valid_ndcs = list(valid_ndcs)
+
+    weights = get(weights, valid_ndcs)
+
+    if isdict:
+        keys = get(keys, valid_ndcs)
+
+    # the total volume is the sum of all weights
+    V_total = sum(weights)
+
+    # prepare array containing the current weight of the bins
+    weight_sum = [0.]
+
+    # iterate through the weight list, starting with heaviest
+    for item, weight in enumerate(weights):
+
+        if isdict:
+            key = keys[item]
+
+        # find candidate bins where the weight might fit
+        candidate_bins = list(filter(lambda i: weight_sum[i] + weight <= V_max, range(len(weight_sum))))
+
+        # if there are candidates where it fits
+        if len(candidate_bins) > 0:
+
+            # find the fullest bin where this item fits and assign it
+            candidate_index = argmax(get(weight_sum, candidate_bins))
+            b = candidate_bins[candidate_index]
+
+        # if this weight doesn't fit in any existent bin
+        elif item > 0:
+            # note! if this is the very first item then there is already an
+            # empty bin open so we don't need to open another one.
+
+            # open a new bin
+            b = len(weight_sum)
+            weight_sum.append(0.)
+            if isdict:
+                bins.append({})
+            else:
+                bins.append([])
+
+        # if we are at the very first item, use the empty bin already open
+        else:
+            b = 0
+
+        # put it in
+        if isdict:
+            bins[b][key] = weight
+        else:
+            bins[b].append(weight)
+
+        # increase weight sum of the bin and continue with
+        # next item
+        weight_sum[b] += weight
+
+    if not is_tuple_list:
+        return bins
+    else:
+        new_bins = []
+        for b in range(len(bins)):
+            new_bins.append([])
+            for _key in bins[b]:
+                new_bins[b].append(new_dict[_key])
+        return new_bins
+
+
+def packing2D(d,
+              V_max,
+              weight_pos=None,
+              key=None,
+              lower_bound=None,
+              upper_bound=None,
+              ):
+    """
+    Distributes a list of weights, a dictionary of weights or a list of tuples containing weights
+    to a minimal number of bins that have a fixed volume.
+
+    Parameters
+    ==========
+    d : iterable
+        list containing weights,
+        OR dictionary where each (key,value)-pair carries the weight as value,
+        OR list of tuples where one entry in the tuple is the weight. The position of
+        this weight has to be given in optional variable weight_pos
+    V_max : int or float
+        Fixed bin volume
+    weight_pos : int, default = None
+        if d is a list of tuples, this integer number gives the position of the weight in a tuple
+    key : function, default = None
+        if d is a list, this key functions grabs the weight for an item
+    lower_bound : float, default = None
+        weights under this bound are not considered
+    upper_bound : float, default = None
+        weights exceeding this bound are not considered
+
+    Returns
+    =======
+    bins : list
+        A list. Each entry is a list of items or
+        a dict of items, depending on the type of ``d``.
+    """
+
+    def get(lst, ndx):
+        return [lst[n] for n in ndx]
+
+    def argmax(lst):
+        return max(range(len(lst)), key=lst.__getitem__)
+
+    def revargsort(lst):
+
+        return sorted(range(len(lst)), key=lambda i: (-lst[i][0], -lst[i][1]))
+
+    isdict = isinstance(d, dict)
+
+    if not hasattr(d, '__len__'):
+        raise TypeError("d must be iterable")
+
+    if not isdict and hasattr(d[0], '__len__'):
+        if weight_pos is not None:
+            key = lambda x: x[weight_pos]
+        if key is None:
+            raise ValueError("Must provide weight_pos or key for tuple list")
+
+    if not isdict and key:
+        new_dict = {i: val for i, val in enumerate(d)}
+        d = {i: key(val) for i, val in enumerate(d)}
+        isdict = True
+        is_tuple_list = True
+    else:
+        is_tuple_list = False
+
+    if isdict:
+
+        # get keys and values (weights)
+        keys_vals = d.items()
+        keys = [k for k, v in keys_vals]
+        vals = [v for k, v in keys_vals]
+
+        # sort weights decreasingly
+        ndcs = revargsort(vals)
+
+        weights = get(vals, ndcs)
+        keys = get(keys, ndcs)
+
+        bins = [{}]
+    else:
+        weights = sorted(d, key=lambda x: -x)
+        bins = [[]]
+
+    # find the valid indices
+    if lower_bound is not None and upper_bound is not None and lower_bound < upper_bound:
+        valid_ndcs = filter(lambda i: lower_bound < weights[i] < upper_bound, range(len(weights)))
+    elif lower_bound is not None:
+        valid_ndcs = filter(lambda i: lower_bound < weights[i], range(len(weights)))
+    elif upper_bound is not None:
+        valid_ndcs = filter(lambda i: weights[i] < upper_bound, range(len(weights)))
+    elif lower_bound is None and upper_bound is None:
+        valid_ndcs = range(len(weights))
+    elif lower_bound >= upper_bound:
+        raise Exception("lower_bound is greater or equal to upper_bound")
+
+    valid_ndcs = list(valid_ndcs)
+
+    weights = get(weights, valid_ndcs)
+
+    if isdict:
+        keys = get(keys, valid_ndcs)
+
+    # the total volume is the sum of all weights
+    V_total = (sum([w[0] for w in weights]), sum([w[1] for w in weights]))
+
+    # prepare array containing the current weight of the bins
+    weight_sum = [(0., 0.)]
+
+    # iterate through the weight list, starting with heaviest
+    for item, weight in enumerate(weights):
+
+        if isdict:
+            key = keys[item]
+
+        # find candidate bins where the weight might fit
+        candidate_bins = list(
+            filter(lambda i: weight_sum[i][0] + weight[0] <= V_max[0] and weight_sum[i][1] + weight[1] <= V_max[1],
+                   range(len(weight_sum))))
+
+        # if there are candidates where it fits
+        if len(candidate_bins) > 0:
+
+            # find the fullest bin where this item fits and assign it
+            candidate_index = argmax(get(weight_sum, candidate_bins))
+            b = candidate_bins[candidate_index]
+
+        # if this weight doesn't fit in any existent bin
+        elif item > 0:
+            # note! if this is the very first item then there is already an
+            # empty bin open so we don't need to open another one.
+
+            # open a new bin
+            b = len(weight_sum)
+            weight_sum.append((0., 0.))
+            if isdict:
+                bins.append({})
+            else:
+                bins.append([])
+
+        # if we are at the very first item, use the empty bin already open
+        else:
+            b = 0
+
+        # put it in
+        if isdict:
+            bins[b][key] = weight
+        else:
+            bins[b].append(weight)
+
+        # increase weight sum of the bin and continue with
+        # next item
+        weight_sum[b] = (weight_sum[b][0] + weight[0], weight_sum[b][1] + weight[1])
+
+    if not is_tuple_list:
+        return bins
+    else:
+        new_bins = []
+        for b in range(len(bins)):
+            new_bins.append([])
+            for _key in bins[b]:
+                new_bins[b].append(new_dict[_key])
+        return new_bins
+
+
+def packing(token_packed, group, src_lens, trg_lens):
+
+    src_group_lens = src_lens[group]
+    trg_group_lens = trg_lens[group]
+    token_max_lens = (src_group_lens.max(), trg_group_lens.max())
+    if token_packed == 'source-target':
+        token_lens_dict = {i: (s, t) for i, s, t in zip(group, src_group_lens, trg_group_lens)}
+        packs = packing2D(token_lens_dict, token_max_lens)
+    elif token_packed == 'source':
+        token_lens_dict = {i: s for i, s in zip(group, src_group_lens)}
+        packs = packing1D(token_lens_dict, token_max_lens[0])
+    elif token_packed == 'target':
+        token_lens_dict = {i: t for i, t in zip(group, trg_group_lens)}
+        packs = packing1D(token_lens_dict, token_max_lens[1])
+    elif token_packed == 'max':
+        token_lens_dict = {i: max(s, t) for i, s, t in zip(group, src_group_lens, trg_group_lens)}
+        packs = packing1D(token_lens_dict, max(token_max_lens))
+
+    return packs
---
+++ padding trick/wmt14/trg/transformer_xiangyu.py
@@ -0,0 +1,329 @@
+import torch
+from torch import nn
+import copy
+import numpy as np
+
+
+def triu_mask(y_mask, device):
+    """x: (xlen, ...)"""
+    q_mask, k_mask = y_mask.T.unsqueeze(-1), y_mask.T.unsqueeze(1)
+    # mask zero-padding
+    zero_attn_mask = k_mask.data.eq(0)  # bx1xsk
+    zero_attn_mask = zero_attn_mask.expand(q_mask.size(0), q_mask.size(1), k_mask.size(2))  # bxsqxsk
+
+    #  triu_attn_mask
+    triu_attn_mask = torch.triu(torch.ones(zero_attn_mask.size(), device=device), 1).bool()
+
+    sent_attn_mat = torch.bmm(q_mask, k_mask)
+    mask_label = np.unique(q_mask.cpu().numpy())
+    mask_label = np.setdiff1d(mask_label, np.array([0]))
+    label_times = np.multiply(mask_label.reshape(1, mask_label.size), mask_label.reshape(mask_label.size, 1))
+    label_times = np.setdiff1d(label_times.flatten(), np.diag(label_times, 0))
+
+    # mask sent-padding
+    sent_attn_mask_total = torch.zeros(sent_attn_mat.size(), dtype=torch.int64, device=device).bool()
+    for label_time in label_times:
+        sent_attn_mask_eq = sent_attn_mat.eq(int(label_time))
+        sent_attn_mask_total += sent_attn_mask_eq
+
+    mask = zero_attn_mask + sent_attn_mask_total + triu_attn_mask
+
+    return mask  # (xlen, xlen)
+
+
+def kv_mask(q_mask, k_mask, device):
+    q_mask, k_mask = q_mask.T.unsqueeze(-1), k_mask.T.unsqueeze(1)
+    # mask zero-padding
+    zero_attn_mask = k_mask.data.eq(0)  # bx1xsk
+    zero_attn_mask = zero_attn_mask.expand(q_mask.size(0), q_mask.size(1), k_mask.size(2))  # bxsqxsk
+
+    mask_label = np.unique(q_mask.cpu().numpy())
+    mask_label = np.setdiff1d(mask_label, np.array([0]))
+    label_times = np.multiply(mask_label.reshape(1, mask_label.size), mask_label.reshape(mask_label.size, 1))
+    label_times = np.setdiff1d(label_times.flatten(), np.diag(label_times, 0))
+
+    # mask sent-padding
+    sent_attn_mat = torch.bmm(q_mask, k_mask)  # bxsqxsk
+    sent_attn_mask_total = torch.zeros(sent_attn_mat.size(), dtype=torch.int64, device=device).bool()
+    for label_time in label_times:
+        sent_attn_mask_eq = sent_attn_mat.eq(int(label_time))
+        sent_attn_mask_total += sent_attn_mask_eq
+
+    mask = zero_attn_mask + sent_attn_mask_total
+    return mask
+
+
+def attn(q, k, v, num_heads, mask=None):
+    """
+    q: (ql, b, dim)
+    k: (kl, b, dim)
+    v: (vl, b, dim)
+    mask: (b, ql, kl)
+    """
+    q = q.transpose(0, 1)
+    k = k.transpose(0, 1)
+    v = v.transpose(0, 1)
+
+    score = (q @ k.transpose(-2, -1)) / (q.size(-1) ** 0.5)  # (b, ql, kl)
+    if mask is not None:
+        mask = torch.cat([mask] * num_heads, 0)
+        score = score.masked_fill(mask, -float('inf'))
+    score = score.softmax(-1)
+    out = (score @ v)
+    out = out.transpose(0, 1)
+    return out, score
+
+
+class NN(nn.Module):
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+
+class Embedding(NN):
+    def __init__(self, vocab_size, dim=512, padding_idx=0, dropout=0.1):
+        super().__init__()
+
+        self.emb = nn.Embedding(vocab_size, dim, padding_idx=padding_idx)
+        nn.init.uniform_(self.emb.weight, -0.01, 0.01)
+        # self.norm = nn.LayerNorm(dim)
+        max_len = 1024
+        pe = torch.zeros(max_len, dim)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, dim, 2).float() *
+            (-torch.tensor(10000.0).log() / dim))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+        self.dropout = nn.Dropout(dropout)
+        self.dim = dim
+
+    def forward(self, x, pos=0):
+        x = self.emb(x) * (self.dim ** 0.5)
+        x = x + self.pe[pos:x.size(0) + pos]
+        x = self.dropout(x)
+        # x = self.norm(x)
+        return x
+
+
+class MultiheadAttention(NN):
+    def __init__(self, dim=512, num_heads=8):
+        super().__init__()
+
+        self.q_in = nn.Linear(dim, dim, bias=False)
+        self.k_in = nn.Linear(dim, dim, bias=False)
+        self.v_in = nn.Linear(dim, dim, bias=False)
+        self.out = nn.Linear(dim, dim, bias=False)
+
+        self.dim = dim
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+
+    def forward(self, q, k, v, mask=None):
+        q_len = q.size(0)
+        k_len = k.size(0)
+        v_len = v.size(0)
+        bsz = q.size(1)
+
+        q = self.q_in(q)
+        k = self.k_in(k)
+        v = self.v_in(v)
+
+        q = q.reshape(q_len, bsz * self.num_heads, self.head_dim)
+        k = k.reshape(k_len, bsz * self.num_heads, self.head_dim)
+        v = v.reshape(v_len, bsz * self.num_heads, self.head_dim)
+
+        out, score = attn(q, k, v, self.num_heads, mask)
+
+        out = out.reshape(q_len, bsz, self.dim)
+        out = self.out(out)
+        return out
+
+
+class FFN(NN):
+    def __init__(self, dim=512, inner_dim=2048):
+        super().__init__()
+
+        self.l1 = nn.Linear(dim, inner_dim)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(inner_dim, dim)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        return x
+
+
+class EncoderLayer(NN):
+    def __init__(self, dim=512, num_heads=8, dropout=0.1):
+        super().__init__()
+
+        self.self_attn = MultiheadAttention(dim, num_heads)
+        self.ffn = FFN(dim, dim * 4)
+
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):  # 2
+        x = x + self.dropout(self.self_attn(x, x, x, mask))  # 3
+        x = self.norm1(x)
+        x = x + self.dropout(self.ffn(x))
+        x = self.norm2(x)
+        return x
+
+
+class DecoderLayer(NN):
+    def __init__(self, dim=512, num_heads=8, dropout=0.1):
+        super().__init__()
+
+        self.self_attn = MultiheadAttention(dim, num_heads)
+        self.ctx_attn = MultiheadAttention(dim, num_heads)
+        self.ffn = FFN(dim, dim * 4)
+
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, y, x, decode_triu_mask, decode_mask):  # 4 y, encode_out, decode_triu_mask, decode_mask
+        """y_step: (ylen, b, dim)
+        x: (xlen, b, dim)"""
+        y = y + self.dropout(self.self_attn(y, y, y, decode_triu_mask))
+        y = self.norm1(y)
+        y = y + self.dropout(self.ctx_attn(y, x, x, decode_mask))  # 5
+        y = self.norm2(y)
+        y = y + self.dropout(self.ffn(y))
+        y = self.norm3(y)
+        return y
+
+    def forward_step(self, y_step, x, cache):
+        """y_step: (1, b, dim)
+        x: (xlen, b, dim)
+        cache: (ylen, b, dim), in which 0 is self_attn_cache, 
+                1 is ctx_attn_cache"""
+
+        cache = y_sofar = torch.cat([cache, y_step])
+        y_step = y_step + self.dropout(self.self_attn(y_step, y_sofar,
+                                                      y_sofar))
+        y_step = self.norm1(y_step)
+
+        y_step = y_step + self.dropout(self.ctx_attn(y_step, x, x))
+        y_step = self.norm2(y_step)
+
+        y_step = y_step + self.dropout(self.ffn(y_step))
+        y_step = self.norm3(y_step)
+
+        return y_step, cache
+
+    def init_cache(self, b, dim):
+        """
+        cache: (ylen=0, b, dim), actually the y_prev
+        """
+        cache = torch.empty(0, b, dim).to(self.device)
+        return cache
+
+
+class Encoder(NN):
+    def __init__(self, layer, num_layers=6):
+        super().__init__()
+
+        self.layers = nn.ModuleList(
+            [copy.deepcopy(layer) for i in range(num_layers)])
+
+    def forward(self, x, mask):  # 6
+        for l in self.layers:
+            x = l(x, mask)  # 7
+        return x
+
+
+class Decoder(NN):
+    def __init__(self, layer, num_layers=6):
+        super().__init__()
+
+        self.layers = nn.ModuleList(
+            [copy.deepcopy(layer) for i in range(num_layers)])
+
+    def forward(self, y, x, decode_triu_mask, decode_mask):  # 8
+        for l in self.layers:
+            y = l(y, x, decode_triu_mask, decode_mask)  # 9
+        return y
+
+    def forward_step(self, y_step, x, cache):
+        """y_step: (1, b, dim)
+        x: (xlen, b, dim)
+        cache: (layer, ylen, b, dim), in which 0 is self_attn_cache, 
+                1 is ctx_attn_cache"""
+        new_cache = []
+        for i, l in enumerate(self.layers):
+            y_step, cache_ = l.forward_step(y_step, x, cache[i])
+            new_cache.append(cache_)
+        return y_step, torch.stack(new_cache)
+
+    def init_cache(self, b, dim):
+        """
+        cache: (layer, ylen=0, b, dim), actually y_prev
+        """
+        num_layers = len(self.layers)
+        cache = torch.empty(num_layers, 0, b, dim).to(self.device)
+        return cache
+
+
+class Transformer(NN):
+    def __init__(self,
+                 src_token_space_size,
+                 trg_token_space_size,
+                 dim=512,
+                 num_layers=6,
+                 num_heads=8,
+                 dropout=0,
+                 pad_id=0,
+                 ):
+        super().__init__()
+
+        self.pad_id = pad_id
+        if pad_id != 0:
+            print('[error] pad_id = {}, but this Transformer model only supports pad_id = 0'.format(pad_id))
+            assert False
+
+        self.dim = dim
+        self.src_emb = Embedding(src_token_space_size,
+                                 dim=dim,
+                                 padding_idx=self.pad_id,
+                                 dropout=dropout)
+        self.trg_emb = Embedding(trg_token_space_size,
+                                 dim=dim,
+                                 padding_idx=self.pad_id,
+                                 dropout=dropout)
+        self.trg_emb.emb.weight = self.src_emb.emb.weight
+        self.proj = nn.Linear(dim, trg_token_space_size, bias=False)
+        self.proj.weight = self.trg_emb.emb.weight
+
+        encoder_layer = EncoderLayer(dim, num_heads, dropout)
+        self.encoder = Encoder(encoder_layer, num_layers)
+        decoder_layer = DecoderLayer(dim, num_heads, dropout)
+        self.decoder = Decoder(decoder_layer, num_layers)
+
+        # hack: name placeholders for loading models from longtu's legacy code; not used at all
+        self.src_tok = None  # src_tok
+        self.trg_tok = None  # trg_tok
+
+    def forward_batch(self, x, y, mask=None):
+
+        if mask is not None:
+            encode_mask = kv_mask(mask[0], mask[0], x.device)
+            decode_triu_mask = triu_mask(mask[1], x.device)
+            decode_mask = kv_mask(mask[1], mask[0], x.device)
+        else:
+            encode_mask, decode_triu_mask, decode_mask = None, None, None
+
+        x = self.src_emb(x)
+        encode_out = self.encoder(x, encode_mask)
+        y = self.trg_emb(y)
+        decode_out = self.decoder(y, encode_out, decode_triu_mask, decode_mask)
+        proj_out = self.proj(decode_out)
+        return proj_out
